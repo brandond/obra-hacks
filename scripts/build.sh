38138e9760eb62c14c3d2f2f039c07530255db10
Brad Davidson <brad.davidson@rancher.com>
2021-09-02 21:53:29 -0700

    add build script
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

---
+++ scripts/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -x
+
+cd $(dirname $0)
+
+DOCKER_ORG=${DOCKER_ORG:-brandond}
+DOCKER_TAG=${DOCKER_TAG:-$(git describe --tags --match 'v[0-9]*' --dirty='-dev')}
+
+docker build -t ${DOCKER_ORG}/obra-hacks:${DOCKER_TAG} ../
